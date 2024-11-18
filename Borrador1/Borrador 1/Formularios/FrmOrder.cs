8eb0c4f20b9eb12d715f1f85bf3f21c7b3e590c5
achitoo <hbaltodano@uamv.edu.ni>
2024-11-17 20:24:30 -0600

    Funcion Pedidos completada, con pequeno bug en el boton editar

--- Borrador1/Borrador 1/Formularios/FrmOrder.cs
+++ Borrador1/Borrador 1/Formularios/FrmOrder.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Borrador_1.Modelos;
 
 namespace Borrador_1.Formularios
 {
@@ -15,13 +16,97 @@ namespace Borrador_1.Formularios
         public FrmOrder()
         {
             InitializeComponent();
+
         }
+        private List<Vehiculo> listaVehiculos = new List<Vehiculo>();
 
         private void btnBackOrderFrmToMain_Click(object sender, EventArgs e)
         {
             FrmMain frm = new FrmMain();
             frm.Show();
             this.Hide();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            // Crear una instancia de Vehiculo
+            Vehiculo vehiculo = new Vehiculo(
+                tbBrand.Text,
+                tbModel.Text,
+                tbDetails.Text,
+                tbYear.Text,
+                tbMontoTotal.Text
+            );
+
+            // Configurar las columnas del DataGridView
+            dgbOrder.Columns.Add("Marca", "Marca");
+            dgbOrder.Columns.Add("Modelo", "Modelo");
+            dgbOrder.Columns.Add("Detalles", "Detalles");
+            dgbOrder.Columns.Add("Año", "Año");
+            dgbOrder.Columns.Add("MontoTotal", "Monto Total");
+
+            // Añadir los datos al DataGridView
+            dgbOrder.Rows.Add(vehiculo.Marca, vehiculo.Modelo, vehiculo.Detalles, vehiculo.Año, vehiculo.MontoTotal);
+
+            // Guardar los datos en la lista
+            listaVehiculos.Add(vehiculo);
+            Clear();
+
+        }
+
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            int indiceFilaSeleccionada = 0;
+
+            if (indiceFilaSeleccionada >= 0)
+            {
+
+                // Actualizar los datos en el DataGridView
+                dgbOrder.Rows[indiceFilaSeleccionada].Cells[0].Value = tbBrand.Text;
+                dgbOrder.Rows[indiceFilaSeleccionada].Cells[1].Value = tbModel.Text;
+                dgbOrder.Rows[indiceFilaSeleccionada].Cells[2].Value = tbDetails.Text;
+                dgbOrder.Rows[indiceFilaSeleccionada].Cells[3].Value = tbYear.Text;
+                dgbOrder.Rows[indiceFilaSeleccionada].Cells[4].Value = tbMontoTotal.Text;
+
+                // Actualizar el objeto en la lista
+                Vehiculo vehiculo = listaVehiculos[indiceFilaSeleccionada];
+                vehiculo.Marca = tbBrand.Text;
+                vehiculo.Modelo = tbModel.Text;
+                vehiculo.Detalles = tbDetails.Text;
+                vehiculo.Año = tbYear.Text;
+                vehiculo.MontoTotal = tbMontoTotal.Text;
+                Clear();
+            }
+
+        }
+
+        public void Clear()
+        {
+            // Limpiar los text boxes
+            tbBrand.Clear();
+            tbModel.Clear();
+            tbDetails.Clear();
+            tbYear.Clear();
+            tbMontoTotal.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int indiceFilaSeleccionada = 0;
+            if (indiceFilaSeleccionada >= 0)
+            {
+                // Eliminar la fila seleccionada del DataGridView
+                dgbOrder.Rows.RemoveAt(indiceFilaSeleccionada);
+
+                // Eliminar el objeto de la lista
+                listaVehiculos.RemoveAt(indiceFilaSeleccionada);
+
+
+
+                indiceFilaSeleccionada = -1;
+            }
         }
     }
 }
